014ecf1493767dcd389e226215c7b580f0406766
Abdullah Jafar <abdulla94jafar@gmail.com>
2022-06-05 15:50:09 +0200

    [UPDATE] creating new project

---
+++ riveAnimation/riveAnimation/riveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  riveAnimationApp.swift
+//  riveAnimation
+//
+//  Created by Abdullah Jaafar on 05.06.22.
+//
+
+import SwiftUI
+
+@main
+struct riveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
